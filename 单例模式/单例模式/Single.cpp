e75337833a6c799beac3386597176c6a111f9d32
kesihai <2693711535@qq.com>
2016-08-21 14:18:01 +0800

    单例模式 添加 饿汉和懒汉区别

--- 单例模式/单例模式/Single.cpp
+++ 单例模式/单例模式/Single.cpp
@@ -10,7 +10,12 @@ Single::~Single()
 	if (cur != NULL) delete cur;
 }
 
-Single* Single:: cur=NULL;
+//饿汉模式的单例模式
+//Single* Single:: cur=Single::GetInstance();
+
+
+//懒汉模式 的单例模式
+Single* Single::cur = NULL;
 
 Single* Single::GetInstance()
 {
