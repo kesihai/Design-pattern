c1d401e268dd0fa982288098b2d41fdeeda86bed
kesihai <2693711535@qq.com>
2016-08-23 13:51:10 +0800

    Revert "天剑 命令模式"
    This reverts commit a6e55894b0dfbae49c2bdee981e4c5a32ab1e4c7.

--- 命令 模式/命令 模式/Receive.h
+++
@@ -1,9 +0,0 @@
-#ifndef _RECEIVE_H_
-#define _RECEIVE_H_
-#include <stdio.h>
-class Receive{
-public:
-	virtual void Excuse() = 0;
-};
-
-#endif
--- 命令 模式/命令 模式/Receive_Fruit.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Receive.h"
-class Receive_Fruit :public Receive
-{
-public:
-	Receive_Fruit();
-	~Receive_Fruit();
-	virtual void Excuse();
-};
